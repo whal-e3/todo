acd266df0fdf9c8f6da3cb16efcebebb3418e881
eric8979 <hsh048148@gmail.com>
2021-03-31 10:20:31 +0900

    Initialize Redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Routes from './components/route/Routes';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route component={Routes} />
-        </Switch>
-      </Router>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Router>
+      </Fragment>
+    </Provider>
   );
 };
 
