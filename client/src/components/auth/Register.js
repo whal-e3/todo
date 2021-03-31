0028ecc8de789d0946cffadb39e7af7722123048
eric8979 <hsh048148@gmail.com>
2021-03-31 10:25:40 +0900

    Add title on Register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -23,61 +23,64 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={e => onSubmit(e)}>
-      <div>
-        <label>Name</label>
-        <input
-          className='u-full-width'
-          type='text'
-          value={name}
-          name='name'
-          onChange={e => onChange(e)}
-          placeholder='Name'
-        />
-      </div>
+    <Fragment>
+      <h2>Sign Up</h2>
+      <form onSubmit={e => onSubmit(e)}>
+        <div>
+          <label>Name</label>
+          <input
+            className='u-full-width'
+            type='text'
+            value={name}
+            name='name'
+            onChange={e => onChange(e)}
+            placeholder='Name'
+          />
+        </div>
 
-      <div>
-        <label>Email</label>
-        <input
-          className='u-full-width'
-          type='text'
-          value={email}
-          name='email'
-          onChange={e => onChange(e)}
-          placeholder='Email Address'
-        />
-      </div>
+        <div>
+          <label>Email</label>
+          <input
+            className='u-full-width'
+            type='text'
+            value={email}
+            name='email'
+            onChange={e => onChange(e)}
+            placeholder='Email Address'
+          />
+        </div>
 
-      <div>
-        <label>password</label>
-        <input
-          className='u-full-width'
-          type='text'
-          value={password}
-          name='password'
-          onChange={e => onChange(e)}
-          placeholder='Enter password'
-        />
-      </div>
+        <div>
+          <label>password</label>
+          <input
+            className='u-full-width'
+            type='text'
+            value={password}
+            name='password'
+            onChange={e => onChange(e)}
+            placeholder='Enter password'
+          />
+        </div>
 
-      <div>
-        <label>password (confirm)</label>
-        <input
-          className='u-full-width'
-          type='text'
-          value={password2}
-          name='password2'
-          onChange={e => onChange(e)}
-          placeholder='Confirm password'
-        />
-      </div>
+        <div>
+          <label>password (confirm)</label>
+          <input
+            className='u-full-width'
+            type='text'
+            value={password2}
+            name='password2'
+            onChange={e => onChange(e)}
+            placeholder='Confirm password'
+          />
+        </div>
 
-      <input
-        className='u-full-width button-primary'
-        type='submit'
-        value='Register'
-      />
-    </form>
+        <input
+          className='u-full-width button-primary'
+          type='submit'
+          value='Register'
+        />
+      </form>
+    </Fragment>
   );
 };
 
