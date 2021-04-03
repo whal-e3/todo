23fc3a94d4bdb35e467433fd7f9a9cab83490940
eric8979 <hsh048148@gmail.com>
2021-04-03 13:42:44 +0900

    Add PrivateRoutes.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -29,7 +29,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
   };
 
   if (isAuthenticated) {
-    return <Redirect to='/' />;
+    return <Redirect to='/todo' />;
   }
 
   return (
--- client/src/components/route/Routes.js
+++ client/src/components/route/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 import PropTypes from 'prop-types';
 
 import Alert from '../layout/Alert';
 import Register from '../auth/Register';
 import Login from '../auth/Login';
+import TodoList from '../todo/TodoList';
 
 const Routes = () => {
   return (
@@ -13,6 +15,7 @@ const Routes = () => {
       <Switch>
         <Route exact path='/register' component={Register} />
         <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/todo' component={TodoList} />
       </Switch>
     </div>
   );
